84bfeb8827b35296531160346debc55bccb0600b
Christian Tucker <149714466+ctucker41@users.noreply.github.com>
2025-11-17 20:15:27 -0500

    Changed routing to align with Vercel until I finish login/signout endpoints

---
+++ frontend/src/app/auth/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function Dashboard() {
+    return (
+        <div className="w-full h-10 sm:h-10 md:h-14 lg:h-14
+        grid grid-cols-3 items-center md:px-6
+        bg-blue-600">
+            <div></div>
+            <h1 className="text-center text-white font-semibold text-xl md:text-2xl select-none">Dashboard</h1>
+            <div className="flex justify-end">
+            <Link href="/" className="text-md md:text-lg text-white hover:bg-blue-700 px-3 py-2 rounded-2xl select-none">Logout</Link>
+            </div>
+        </div>
+
+    )
+}
