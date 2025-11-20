7c7fa631b4f602660171954e312053a73a14a39b
Christian Tucker <149714466+ctucker41@users.noreply.github.com>
2025-11-19 20:06:26 -0500

    Fixed rerouting page for dashboard authorization

--- frontend/src/app/auth/dashboards/page.tsx
+++ frontend/src/app/auth/dashboards/page.tsx
@@ -13,7 +13,7 @@ export default function Dashboard() {
         const token = localStorage.getItem("token");
 
         if (!token) {
-            router.replace("/auth/login");
+            router.replace("/");
         } else {
             setCheckingAuth(false);
         }
